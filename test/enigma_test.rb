838b963d184950b19d7cd782f3cc01c7522cab79
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-05-13 14:43:32 -0600

    added enigma implementation and test files

---
+++ test/enigma_test.rb
@@ -0,0 +1,17 @@
+require 'simplecov'
+SimpleCov.start
+require 'pry'
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/enigma'
+require './lib/encrypt'
+require './lib/decrypt'
+require './lib/crack'
+
+class Enigma < Minitest::Test
+  def test_enigma_instance_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
+end
