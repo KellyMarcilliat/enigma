6882eb8d13815193766f7c5c74d436e38a0adeaf
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-05-15 18:28:58 -0600

    WIP - crack tested through reverse_date_offset

--- lib/crack.rb
+++ lib/crack.rb
@@ -4,26 +4,37 @@ class Crack
   def initialize
   end
 
-
-
-#CRACKING A KEY
-#When the key is not known, the offsets can still be calculated from the message date. We believe that each enemy message ends with the characters "..end..". Use that to determine when you’ve correctly guessed the key.
-
-# "..end.." Ends every message
 # pull last 7 characters of message
-  # message_array = string.reverse.split
-  # ending_array = message_array[0..6]
-    # this is what will be evaluated
-# calculate offsets from date
-  # given_date ** 2
-  # last 4 digits
+  def last_seven_characters(message)
+    last_seven_array = []
+    last_seven_array = message.split("")[-7..-1]
+  end
+
+  # calculate reverse offset from date
+  def reverse_date_offset(date)
+    date_integer = date.to_i
+    date_squared = date_integer ** 2
+    date_offset = date_squared.digits[0..3].reverse
+  end
+
 # rotate message_array characters backwards per offsets
-  # adjusted_array = message_array.backward_offset
-# convert 10,000 to [01, 12, 23, 34]
-# run that through rotator
-# if result is not "..end.."
+  # adjusted_message = message_array.backward_offset
+  # adjusted_message  adjusted_array
+# does adjusted message == "..end.."
+  #
+  # check adjusted_array against [00, 00, 00, 00]
+  # convert 10,000 to [01, 12, 23, 34]
+    # def converter
+      # key = []
+      # key << five_digit_array[0] * 10 + five_digit_array[1]
+      # key << five_digit_array[1] * 10 + five_digit_array[2]
+      # key << five_digit_array[2] * 10 + five_digit_array[3]
+      # key << five_digit_array[3] * 10 + five_digit_array[4]
+      # key
+    # end
+  # run adjusted_array backwards that amount (Pat's code)
+  # if result is not "..end.."
 # convert 10,001 to
+end
 
-## Idea:  if rotating forward doesn't work, rotate backwards (on the 10,000 and up thing)
-
-end 
+## Idea:  if rotating backwards doesn't work, rotate forward (on the 10,000 and up thing)
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 require 'pry'
+require 'date'
 require "minitest/autorun"
 require "minitest/pride"
 require './lib/crack'
@@ -8,7 +9,16 @@ require './lib/crack'
 class CrackTest < Minitest::Test
   def test_crack_instance_exists
     crack = Crack.new
-
     assert_instance_of Crack, crack
+  end
+
+  def test_last_seven_characters_arrays_last_7_char
+    crack = Crack.new
+    assert_equal [".", ".", "e", "n", "d", ".", "."], crack.last_seven_characters("amazing message ..end..")
+  end
+
+  def test_reverse_date_offset_arrays_last_4_digits
+    crack = Crack.new
+    assert_equal [8, 3, 2, 4], crack.reverse_date_offset("150518")
   end
 end
